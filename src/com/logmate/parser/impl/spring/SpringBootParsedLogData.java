ab8bed75a644501d189d3bb4337fe9264408ee31
kangchanwook <kan0202@naver.com>
2025-04-09 15:32:07 +0900

    [Feat] ParsedLogData 구현체 개발 - SpringBootParsedLogData

---
+++ src/com/logmate/parser/impl/spring/SpringBootParsedLogData.java
@@ -0,0 +1,42 @@
+package com.logmate.parser.impl.spring;
+
+
+import java.time.LocalDateTime;
+import com.logmate.parser.ParsedLogData;
+
+public class SpringBootParsedLogData implements ParsedLogData {
+  private boolean isFormatCorrect;
+  private LocalDateTime timestamp;
+  private String level;
+  private String thread;
+  private String logger;
+  private String message;
+
+  public SpringBootParsedLogData(boolean isFormatCorrect, LocalDateTime timestamp, String level, String thread,
+      String logger,
+      String message) {
+    this.isFormatCorrect = isFormatCorrect;
+    this.timestamp = timestamp;
+    this.level = level;
+    this.thread = thread;
+    this.logger = logger;
+    this.message = message;
+  }
+
+  @Override
+  public String getMessage() {
+    return message;
+  }
+
+  @Override
+  public String toString() {
+    return "{" +
+        "\"isFormatCorrect\":" + isFormatCorrect +
+        ",\"timestamp\":\"" + timestamp + '\"' +
+        ",\"level\":\"" + level + '\"' +
+        ",\"thread\":\"" + thread + '\"' +
+        ",\"logger\":\"" + logger + '\"' +
+        ",\"message\":\"" + message + '\"' +
+        '}';
+  }
+}
