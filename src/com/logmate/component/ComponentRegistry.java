abe4b2a47c11105d8b1ab27483a89e7d1b53eb42
kangchanwook <kan0202@naver.com>
2025-04-09 15:33:40 +0900

    [Feat] ComponentRegistry 구현
    System 에서 사용하는 컴포넌트들을 생성하고, Dependency 를 연결시키는 Registry 구현

---
+++ src/com/logmate/component/ComponentRegistry.java
@@ -0,0 +1,77 @@
+package com.logmate.component;
+
+
+import com.logmate.exporter.LogExporter;
+import com.logmate.exporter.impl.ConsoleLogExporter;
+import com.logmate.filter.LogFilter;
+import com.logmate.filter.impl.NonLogFilter;
+import java.io.File;
+import java.util.Objects;
+import com.logmate.parser.LogParser;
+import com.logmate.parser.impl.spring.SpringBootLogParser;
+import com.logmate.watcher.listener.LogEventListener;
+import com.logmate.watcher.listener.impl.DefaultLogEventListener;
+import com.logmate.watcher.tailer.LogTailer;
+import com.logmate.watcher.tailer.impl.FileLogTailer;
+
+/**
+ * System 에서 사용하는 컴포넌트들을 생성하고, Dependency 를 연결한다.
+ *
+ */
+public class ComponentRegistry {
+
+  private String filePath;
+  private LogTailer logTailer;
+  private LogEventListener logEventListener;
+  private LogParser logParser;
+  private LogFilter logFilter;
+  private LogExporter logExporter;
+
+  public ComponentRegistry(String filePath) {
+    this.filePath = filePath;
+  }
+
+  public LogTailer getLogTailer() {
+    if (Objects.isNull(logTailer)) {
+      logTailer = new FileLogTailer(new File(filePath), getLogEventListener());
+      return logTailer;
+    }
+    return logTailer;
+  }
+
+  public LogEventListener getLogEventListener() {
+    if (Objects.isNull(logEventListener)) {
+      logEventListener = new DefaultLogEventListener(
+          getLogParser(),
+          getLogFilter(),
+          getLogExporter()
+      );
+      return logEventListener;
+    }
+    return logEventListener;
+  }
+
+  public LogParser getLogParser() {
+    if (Objects.isNull(logParser)) {
+      logParser = new SpringBootLogParser();
+      return logParser;
+    }
+    return logParser;
+  }
+
+  public LogFilter getLogFilter() {
+    if (Objects.isNull(logFilter)) {
+      logFilter = new NonLogFilter();
+      return logFilter;
+    }
+    return logFilter;
+  }
+
+  public LogExporter getLogExporter() {
+    if (Objects.isNull(logExporter)) {
+      logExporter = new ConsoleLogExporter();
+      return logExporter;
+    }
+    return logExporter;
+  }
+}
