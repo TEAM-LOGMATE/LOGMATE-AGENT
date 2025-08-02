ad226e68014593a9ba398957ead1e62f8f18cdb8
chanwookK <kan0202@naver.com>
2025-08-02 21:23:01 +0900

    [Feat] Main ConfigPullerRunManager 적용

--- src/main/java/com/logmate/Main.java
+++ src/main/java/com/logmate/Main.java
@@ -1,6 +1,6 @@
 package com.logmate;
 
-import com.logmate.injection.server.ConfigInjectionServer;
+import com.logmate.injection.puller.ConfigPullerRunManager;
 import com.logmate.tailer.TailerRunManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,8 +9,9 @@ public class Main {
   private static final Logger log = LoggerFactory.getLogger(Main.class);
 
   public static void main(String[] args) throws Exception {
-    ConfigInjectionServer.start(2373);
+    //ConfigInjectionServer.start(2373);
+    ConfigPullerRunManager.start();
     TailerRunManager.start();
-    log.info("log-mate-watcher is running");
+    log.info("tail-mate is running");
   }
 }
