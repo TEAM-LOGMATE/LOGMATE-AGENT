d85ed67fb9aad510307a4d25eefd41a286bfb4f4
chanwookK <kan0202@naver.com>
2025-08-03 15:14:33 +0900

    [Feat] Agent, Puller, Watcher 3가지 설정으로 분리. 이후 Config Loader 수정

--- src/main/java/com/logmate/injection/config/util/YamlConfigLoader.java
+++ src/main/java/com/logmate/injection/config/util/YamlConfigLoader.java
@@ -1,5 +1,7 @@
 package com.logmate.injection.config.util;
 
+import com.logmate.injection.config.AgentConfig;
+import com.logmate.injection.config.PullerConfig;
 import com.logmate.injection.config.WatcherConfig;
 import java.io.InputStream;
 import org.yaml.snakeyaml.LoaderOptions;
@@ -8,11 +10,27 @@ import org.yaml.snakeyaml.constructor.Constructor;
 
 public class YamlConfigLoader {
 
-  public static WatcherConfig load(String path) {
+  public static WatcherConfig loadTailerConfig() {
     Yaml yaml = new Yaml(new Constructor(WatcherConfig.class, new LoaderOptions()));
     InputStream inputStream = YamlConfigLoader.class
         .getClassLoader()
-        .getResourceAsStream(path);
+        .getResourceAsStream("default-tailer-config.yml");
+    return yaml.load(inputStream);
+  }
+
+  public static PullerConfig loadPullerConfig() {
+    Yaml yaml = new Yaml(new Constructor(PullerConfig.class, new LoaderOptions()));
+    InputStream inputStream = YamlConfigLoader.class
+        .getClassLoader()
+        .getResourceAsStream("default-puller-config.yml");
+    return yaml.load(inputStream);
+  }
+
+  public static AgentConfig loadAgentConfig() {
+    Yaml yaml = new Yaml(new Constructor(AgentConfig.class, new LoaderOptions()));
+    InputStream inputStream = YamlConfigLoader.class
+        .getClassLoader()
+        .getResourceAsStream("default-agent-config.yml");
     return yaml.load(inputStream);
   }
 }
