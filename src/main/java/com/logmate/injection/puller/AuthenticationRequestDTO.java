a39f87d01a31177c2d9ae77fbfe0e380c7ef13af
kangchanwook <kan0202@naver.com>
2025-08-05 01:03:12 +0900

    [Feat] Puller Client DTO 추가

---
+++ src/main/java/com/logmate/injection/puller/AuthenticationRequestDTO.java
@@ -0,0 +1,12 @@
+package com.logmate.injection.puller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AuthenticationRequestDTO {
+  String agentId;
+}
---
+++ src/main/java/com/logmate/injection/puller/TokenDTO.java
@@ -0,0 +1,12 @@
+package com.logmate.injection.puller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TokenDTO {
+  String accessToken;
+}
