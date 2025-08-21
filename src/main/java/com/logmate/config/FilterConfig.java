67dac9aa099f6a8e1c401d7b8d53d82a51163a16
chanwookK <kan0202@naver.com>
2025-08-21 23:20:30 +0900

    [Fix] FilterConfig Set 초기화 하도록 수정

--- src/main/java/com/logmate/config/FilterConfig.java
+++ src/main/java/com/logmate/config/FilterConfig.java
@@ -1,6 +1,7 @@
 package com.logmate.config;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,13 +14,13 @@ import lombok.NoArgsConstructor;
 public class FilterConfig {
 
   // Spring boot 전용 필터 항목
-  private Set<String> allowedLevels;
-  private Set<String> allowedLoggers;
-  private Set<String> requiredKeywords;
+  private Set<String> allowedLevels = new HashSet<>();
+  private Set<String> allowedLoggers = new HashSet<>();
+  private Set<String> requiredKeywords = new HashSet<>();
   private LocalDateTime after;
 
   // 웹로그 전용 필터 항목
-  private Set<String> allowedMethods;       // GET, POST 등
-  private Set<Integer> allowedStatusCodes;  // 200, 500 등
-  private Set<String> urlPrefix;                 // 특정 경로로 시작하는 URL만 허용
+  private Set<String> allowedMethods = new HashSet<>();       // GET, POST 등
+  private Set<Integer> allowedStatusCodes  = new HashSet<>();  // 200, 500 등
+  private Set<String> urlPrefix = new HashSet<>();                 // 특정 경로로 시작하는 URL만 허용
 }
