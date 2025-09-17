b245e1a7d8fd99c9020a1c1f8e2e6faba4da8c08
chanwookK <kan0202@naver.com>
2025-09-17 11:16:17 +0900

    [Refactor] Main 에서 Initializer 를 생성하도록 변경

--- src/main/java/com/logmate/Main.java
+++ src/main/java/com/logmate/Main.java
@@ -1,33 +1,13 @@
 package com.logmate;
 
-import com.logmate.config.holder.AgentConfigHolder;
-import com.logmate.config.puller.ConfigPullerRunManager;
-import java.util.Arrays;
-import java.util.Optional;
+import com.logmate.bootstrap.AgentInitializer;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Main {
 
   public static void main(String[] args) {
-    String agentId = null;
-
-    // args 배열에서 --agentId=value 형태의 인자를 찾음
-    Optional<String> agentIdArg = Arrays.stream(args)
-        .filter(arg -> arg.startsWith("--agentId="))
-        .findFirst();
-
-    if (agentIdArg.isPresent()) {
-      // --agentId= 뒤의 값을 추출
-      agentId = agentIdArg.get().substring("--agentId=".length());
-      log.info("Argument agentId: {}", agentId);
-    } else {
-      // 인자가 없을 경우 기본값 설정
-      agentId = "agent-uuid1";
-      log.warn("Argument agentId is not found. Using default agentId: {}", agentId);
-    }
-
-    AgentConfigHolder.get().setAgentId(agentId);
-    ConfigPullerRunManager.start();
+    AgentInitializer initializer = new AgentInitializer();
+    initializer.init(args);
   }
 }
