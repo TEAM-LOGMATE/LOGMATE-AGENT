f5c277e44f6961a43df261086e412d4dc7694bc8
kangchanwook <kan0202@naver.com>
2025-10-16 20:10:32 +0900

    [Feat] LogType을 통한 Guice Registry 주입 로직 구현. Fallback Storage Guice Registry 에 적용

--- src/main/java/com/logmate/di/LogPipelineComponentRegistry.java
+++ src/main/java/com/logmate/di/LogPipelineComponentRegistry.java
@@ -4,19 +4,20 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.logmate.config.data.AgentConfig;
 import com.logmate.config.data.pipeline.LogPipelineConfig;
-import com.logmate.processor.filter.impl.TomcatAccessLogFilter;
-import com.logmate.processor.parser.impl.web.TomcatAccessLogParser;
+import com.logmate.processor.exporter.impl.ConsoleLogExporter;
+import com.logmate.processor.fallback.FallbackStorage;
+import com.logmate.processor.fallback.impl.FileFallbackStorage;
+import com.logmate.processor.parser.LogType;
 import com.logmate.tailer.LogTailer;
 import com.logmate.processor.exporter.LogExporter;
 import com.logmate.processor.exporter.impl.HttpLogExporter;
 import com.logmate.processor.filter.LogFilter;
-import com.logmate.processor.filter.impl.SpringBootLogFilter;
 import com.logmate.tailer.impl.FileLogTailer;
 import com.logmate.processor.listener.LogEventListener;
 import com.logmate.processor.listener.impl.DefaultLogEventListener;
 import com.logmate.processor.merger.MultilineProcessor;
 import com.logmate.processor.parser.LogParser;
-import com.logmate.processor.parser.impl.spring.SpringBootLogParser;
+import org.slf4j.MDC;
 
 public class LogPipelineComponentRegistry extends AbstractModule {
 
@@ -30,22 +31,20 @@ public class LogPipelineComponentRegistry extends AbstractModule {
 
   @Override
   protected void configure() {
-    switch (logPipelineConfig.getParser().getType()) {
-      case "springboot":
-        bind(LogParser.class).toInstance(new SpringBootLogParser(logPipelineConfig.getParser()));
-        bind(LogFilter.class).toInstance(new SpringBootLogFilter(logPipelineConfig.getFilter()));
-        break;
-      case "tomcat":
-        bind(LogParser.class).toInstance(new TomcatAccessLogParser(logPipelineConfig.getParser()));
-        bind(LogFilter.class).toInstance(new TomcatAccessLogFilter(logPipelineConfig.getFilter()));
-        break;
-      default:
-        bind(LogParser.class).toInstance(new SpringBootLogParser(logPipelineConfig.getParser()));
-        bind(LogFilter.class).toInstance(new SpringBootLogFilter(logPipelineConfig.getFilter()));
-    }
+    LogType logType = LogType.valueOf(logPipelineConfig.getParser().getType().toUpperCase());
+    bind(LogParser.class).toInstance(
+        logType.createParser(logPipelineConfig.getParser())
+    );
+    bind(LogFilter.class).toInstance(
+        logType.createFilter(logPipelineConfig.getFilter())
+    );
     bind(LogExporter.class).toInstance(
-        //new ConsoleLogExporter());
-        new HttpLogExporter(logPipelineConfig.getExporter(), agentconfig));
+        //new ConsoleLogExporter()
+        new HttpLogExporter(logPipelineConfig.getExporter(), agentconfig)
+    );
+    bind(FallbackStorage.class).toInstance(
+        new FileFallbackStorage(logPipelineConfig.getFallbackStorage())
+    );
   }
 
   @Provides
@@ -68,7 +67,7 @@ public class LogPipelineComponentRegistry extends AbstractModule {
 
   @Provides
   public LogEventListener provideLogEventListener(LogExporter exporter, LogFilter filter,
-      LogParser parser, MultilineProcessor multilineProcessor) {
-    return new DefaultLogEventListener(parser, filter, exporter, multilineProcessor);
+      LogParser parser, MultilineProcessor multilineProcessor, FallbackStorage fallbackStorage) {
+    return new DefaultLogEventListener(parser, filter, exporter, multilineProcessor, fallbackStorage);
   }
 }
