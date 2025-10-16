04f24eebc8812c10eeba079434ba48162e086fde
kangchanwook <kan0202@naver.com>
2025-10-16 20:25:01 +0900

    [Refactor] Main 에서 Guice 를 이용해 실행하도록 변경

--- src/main/java/com/logmate/Main.java
+++ src/main/java/com/logmate/Main.java
@@ -1,13 +1,17 @@
 package com.logmate;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import com.logmate.bootstrap.AgentInitializer;
+import com.logmate.di.AgentInitializerRegistry;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Main {
 
   public static void main(String[] args) {
-    AgentInitializer initializer = new AgentInitializer();
+    Injector injector = Guice.createInjector(new AgentInitializerRegistry());
+    AgentInitializer initializer = injector.getInstance(AgentInitializer.class);
     initializer.init(args);
   }
 }
