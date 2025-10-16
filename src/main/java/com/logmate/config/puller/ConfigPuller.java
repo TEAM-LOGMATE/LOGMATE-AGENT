9e40a5fdc9d776d29992e3c5507a15ec204bbb30
kangchanwook <kan0202@naver.com>
2025-10-16 20:24:23 +0900

    [Feat] AccessToken 이 만료 되었을 경우 재요청하는 로직 구현

--- src/main/java/com/logmate/config/puller/ConfigPuller.java
+++ src/main/java/com/logmate/config/puller/ConfigPuller.java
@@ -1,56 +1,76 @@
 package com.logmate.config.puller;
 
+import com.logmate.bootstrap.args.AgentArguments;
+import com.logmate.bootstrap.auth.AgentAuthenticator;
 import com.logmate.config.data.AgentConfig;
 import com.logmate.config.data.PullerConfig;
 import com.logmate.config.holder.AgentConfigHolder;
 import com.logmate.config.holder.PullerConfigHolder;
-import com.logmate.config.puller.dto.ConfigDTO;
-import java.util.Optional;
+import com.logmate.config.puller.dto.ConfigPullResponse;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ConfigPuller implements Runnable {
 
-  private final ConfigPullerClient client;
+  private final ConfigPullClient client;
   private final ConfigUpdater updater;
+  private final AgentAuthenticator authenticator;
+  private final AgentArguments agentArguments;
   private String etag;
 
-  public ConfigPuller(ConfigPullerClient client, ConfigUpdater updater) {
+
+  public ConfigPuller(ConfigPullClient client, ConfigUpdater updater,
+      AgentAuthenticator authenticator, AgentArguments agentArguments) {
     this.client = client;
     this.updater = updater;
+    this.authenticator = authenticator;
+    this.agentArguments = agentArguments;
     this.etag = String.valueOf(UUID.randomUUID());
   }
 
   @Override
   public void run() {
-    // 2. 주기적으로 Config 서버로부터 설정 Pull
+    // 주기적으로 Config 서버로부터 설정 Pull
     while (true) {
+      // Global 한 설정을 다시 가져온다.
       AgentConfig agentConfig = AgentConfigHolder.get();
       PullerConfig pullerConfig = PullerConfigHolder.get();
 
-      String baseUrl = pullerConfig.getPullURL() + "/config";
-      /*String query = String.format(
-          "eTag=%s",
-          URLEncoder.encode(etag, StandardCharsets.UTF_8)
+      // puller url (API Server)
+      String baseUrl = pullerConfig.getPullURL();
+      String query = String.format(
+          "agentId=%s&etag=%s",
+          agentConfig.getAgentId(),
+          etag
       );
       String requestURL = baseUrl + "?" + query;
 
-
-       */
       // HTTPs 요청
-      Optional<ConfigDTO> response = client.configPull(baseUrl, agentConfig.getAccessToken());
+      ConfigPullResponse response = client.configPull(requestURL, agentConfig.getAccessToken());
+
 
       // Config 에 변경점이 있음
-      if (response.isPresent()) {
-        log.info("[ConfigPuller] Received new configuration update.");
-        updater.apply(response.get());
-        etag = response.get().getEtag();
-        log.info("[ConfigPuller] ETag updated: {}", etag);
-        log.info("[ConfigPuller] Configuration puller completed.");
-      } else {
+      if (response.getStatus() == 200) {
+        log.info("[ConfigPuller] Received new configuration.");
+        updater.apply(response.getBody());
+        etag = response.getBody().getEtag();
+        log.info("[ConfigPuller] Configuration updated.");
+      }
+      else if (response.getStatus() == 401) {
+        authenticator.authenticate(agentArguments);
+        log.info("[ConfigPuller] Authentication succeeded. Retrying pull...");
+        ConfigPullResponse retryResponse = client.configPull(baseUrl, agentConfig.getAccessToken());
+        if (retryResponse.getStatus() == 200) {
+          log.info("[ConfigPuller] Retry succeeded. Updating configuration...");
+          // todo: validation 먼저 하는 것 필요
+          updater.apply(retryResponse.getBody());
+          etag = retryResponse.getBody().getEtag();
+        }
+      }
+      else if (response.getStatus() == 304) {
         // Config에 변경 없음
-        log.debug("[ConfigPuller] No config changes. ETag=, continuing...");
+        log.debug("[ConfigPuller] No config changes. continuing...");
       }
 
       try {
--- src/main/java/com/logmate/config/puller/ConfigPullerRunManager.java
+++ src/main/java/com/logmate/config/puller/ConfigPullerRunManager.java
@@ -1,5 +1,7 @@
 package com.logmate.config.puller;
 
+import com.logmate.bootstrap.args.AgentArguments;
+import com.logmate.bootstrap.auth.AgentAuthenticator;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,9 +11,10 @@ public class ConfigPullerRunManager {
 
   private Thread pullerThread;
 
-  public void start() {
+  public void start(AgentAuthenticator agentAuthenticator, AgentArguments agentArguments) {
     pullerThread = new Thread(
-        new ConfigPuller(new ConfigPullerClient(), new ConfigUpdater())
+        new ConfigPuller(new ConfigPullClient(), new ConfigUpdater(new ConfigConverter()),
+            agentAuthenticator, agentArguments)
     );
     pullerThread.start();
     log.info("[ConfigPullerRunManager] configuration puller started...");
